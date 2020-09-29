563814a66d39f99ba8636aa1ffe1342f6a82d874
TANVIR ISLAM <mdtanvirislams@gmail.com>
2020-09-30 00:46:33 +0600

    solving installing problem

---
+++ src/actionCreators/authM.js
@@ -0,0 +1,36 @@
+export const SIGNUP = 'SIGNUP';
+
+  export const signup = (email, password) => {
+     return async dispatch => {
+         try {
+             const response = await fetch(
+                 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCvib1iZmXGjoYLtcRbhLO-D3l-O-aflCY', 
+                 {
+                     method: 'POST',
+                     headers: {
+                         'Content-Type': 'application/json'
+                     },
+                     body: JSON.stringify({
+                         email: email,
+                         password: password,
+                         returnSecureToken: true
+                     })
+                 }
+             );
+
+              if (!response.ok) {
+                 throw new Error('Something went wrong 1!');
+             }
+
+              const resData = await response.json();
+             console.log(resData);
+
+              dispatch({ type: SIGNUP });
+         } catch (err) {
+             console.log('printing error: ');
+             console.log(err);
+             console.log('finishing error');
+             throw err;
+         }
+     }
+ } 
---
+++ src/screens/AuthScreen.js
@@ -0,0 +1,147 @@
+import React, { useReducer, useCallback } from 'react';
+ import { View, ScrollView, StyleSheet, KeyboardAvoidingView, Button } from 'react-native';
+ import { LinearGradient } from 'expo-linear-gradient';
+ import { useDispatch } from 'react-redux';
+
+  import Input from '../../components/UI/Input';
+ import Card from '../../components/UI/Card';
+ import { ScrollView } from 'react-native-gesture-handler';
+ import Colors from '../../constants/Colors';
+import * as authActions from '../actionCreators/authM';
+
+ const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+
+  const formReducer = (state, action) => {
+     if (action.type === FORM_INPUT_UPDATE) {
+         const updatedValues = {
+             ...state.inputValues,
+             [action.input]: action.value
+         };
+         const updatedValidities = {
+             ...state.inputValidities,
+             [action.input]: action.isValid
+         }
+         let updatedFormIsValid = true;
+         for (const key in updatedValidities) {
+             updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+         }
+         return {
+             formIsValid: updatedFormIsValid,
+             inputValidities: updatedValidities,
+             inputValues: updatedValues
+         };
+     }
+     return state;
+ }
+
+  const AuthScreen = props => {
+    const dispatch = useDispatch();
+    // const [state, dispatch] = useReducer(reducer, initialState);
+    const [formState, dispatchFormState ] =  useReducer(formReducer, {
+       inputValues: {
+           email: '',
+           password: ''
+       }, 
+       inputValidities: {
+           email: false,
+           password: false
+       }, 
+       formIsValid: false
+   });
+
+    const signUpHandler = () => {
+       console.log('entering signup');
+       dispatch(authActions.signup(
+           formState.inputValues.email,
+           formState.inputValues.password
+       ));
+       console.log('finishing signup action');
+   }
+
+    const inputChangeHandler = useCallback((inputIdentifier, inputValue, inputValidity) => {
+       dispatchFormState({
+           type: FORM_INPUT_UPDATE, 
+           value: inputValue,
+           isValid: inputValidity,
+           input: inputIdentifier//'title'
+       });
+   }, [dispatchFormState]);
+     return ( 
+         <KeyboardAvoidingView 
+             behavior='padding'
+             keyboardVerticalOffset={50}
+             style={styles.screen}
+         >
+             <LinearGradient colors={['#ffedff','#ffe3ff']} style={styles.gradient}>
+                 <Card style={styles.authContainer}>
+                     <ScrollView>
+                         <Input 
+                             id="email" 
+                             label="E-Mail" 
+                             keyboardType="email-address"
+                             required
+                             email
+                             autoCapitalize="none"
+                             errorText="Please enter a valid email address."
+                             onInputChange={inputChangeHandler}
+                             initialValue=""
+                         />
+                         <Input 
+                             id="password" 
+                             label="Password" 
+                             keyboardType="default"
+                             secureTextEntry
+                             required
+                             minLength={5}
+                             autoCapitalize="none"
+                             errorText="Please enter a valid password."
+                             onInputChange={inputChangeHandler}
+                             initialValue=""
+                         />
+                         <View style={styles.buttonContainer}>
+                             <Button 
+                                title="Login" 
+                                color={Colors.primary} 
+                                onPress={signupHandler}
+                             />
+                         </View>
+                         <View style={styles.buttonContainer}>
+                             <Button 
+                                 title="Switch to Sign Up" 
+                                 color={Colors.accent} 
+                                 onPress={() => {}}
+
+                              />
+                         </View>
+                     </ScrollView>
+                 </Card>
+             </LinearGradient>
+         </KeyboardAvoidingView>
+     )
+ };
+
+  AuthScreen.navigationOptions = {
+     headerTitle: 'Authenticate'
+ }
+
+  const styles = StyleSheet.create({
+     screen: { 
+         flex:1
+     },
+     gradient: {
+         flex: 1,
+         justifyContent: 'center',
+         alignItems: 'center'
+     },
+     authContainer: {
+         width: '80%',
+         maxWidth: 400,
+         maxHeight: 400,
+         padding: 20
+     },
+     buttonContainer: {
+         marginTop: 10,
+     }
+ });
+
+  export default AuthScreen;
